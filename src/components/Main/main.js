9360345e1ecc19c52ef1721babb35febc5057794
shrutibakutra <shruti15bakutra@gmail.com>
2026-04-01 17:39:48 +0200

    upadte app name

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -23,7 +23,7 @@ const Main = () => {
         <div className="main-container">
         <div className='main'>
             <div className='main__left-end'>
-                <h1 className='main__left-end__title'> Welcome to ThriillApp!</h1>
+                <h1 className='main__left-end__title'> Welcome to Thriill! Train Your Ear!</h1>
                 <div className='main__left-end__sub-title'>Unlock the world of music theory, from notations to intervals, in a fun and engaging way—right at your fingertips. Ready to level up your musical skills? Sign up and start the journey today!</div>
                 <div className='main__left-end__buttons'>
                 <a href="https://apps.apple.com/in/app/thriill-train-your-ear/id6744299502" target="_blank" rel="noopener noreferrer">
